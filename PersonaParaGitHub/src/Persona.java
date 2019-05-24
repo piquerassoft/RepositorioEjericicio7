4fa9d02061380777ae6ba1df682954ebd5db49ff
jjimenez19 <josemanueljimenezpiqueras@gmail.com>
2019-05-24 12:59:33 +0200

    Primera Subida

---
+++ PersonaParaGitHub/src/Persona.java
@@ -0,0 +1,18 @@
+
+public class Persona {
+		private String dni;
+		private String nombre;
+		private String apellidos;
+		private String direccion;
+		private int edad;
+		public Persona(String dni, String nombre, String apellidos, String direccion, int edad) {
+			super();
+			this.dni = dni;
+			this.nombre = nombre;
+			this.apellidos = apellidos;
+			this.direccion = direccion;
+			this.edad = edad;
+		}
+		
+		
+}
